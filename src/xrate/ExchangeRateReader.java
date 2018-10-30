90dbd16a11e537985d9c86dd88e907ace84ceb2a
SirLich <lrkoehler77@gmail.com>
2018-10-30 11:14:46 -0500

    Merged changes from various needed repos. Add padWithZero method. Update how the getExchange methods parse URLs. All tests pass.

--- src/xrate/ExchangeRateReader.java
+++ src/xrate/ExchangeRateReader.java
@@ -2,16 +2,10 @@ package xrate;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.sun.org.apache.xerces.internal.xs.StringList;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.lang.reflect.Array;
+import java.io.*;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Properties;
 
 /**
  * Provide access to basic currency exchange rate services.
@@ -21,6 +15,7 @@ import java.util.List;
 public class ExchangeRateReader {
 
     private String base;
+    private String accessKey;
 
     /**
      * Construct an exchange rate reader using the given base URL. All requests
@@ -33,14 +28,54 @@ public class ExchangeRateReader {
      * @param baseURL
      *            the base URL for requests
      */
-    public ExchangeRateReader(String baseURL) {
+    public ExchangeRateReader(String baseURL) throws IOException {
         base = baseURL;
+
+        readAccessKeys();
+    }
+
+    /**
+     * This reads the `fixer_io` access key from `etc/access_keys.properties`
+     * and assigns it to the field `accessKey`.
+     *
+     * @throws IOException if there is a problem reading the properties file
+     */
+    private void readAccessKeys() throws IOException {
+        Properties properties = new Properties();
+        FileInputStream in = null;
+        try {
+            // Don't change this filename unless you know what you're doing.
+            // It's crucial that we don't commit the file that contains the
+            // (private) access keys. This file is listed in `.gitignore` so
+            // it's safe to put keys there as we won't accidentally commit them.
+            in = new FileInputStream("etc/access_keys.properties");
+        } catch (FileNotFoundException e) {
+            /*
+             * If this error gets generated, make sure that you have the desired
+             * properties file in your project's `etc` directory. You may need
+             * to rename the file ending in `.sample` by removing that suffix.
+             */
+            System.err.println("Couldn't open etc/access_keys.properties; have you renamed the sample file?");
+            throw(e);
+        }
+        properties.load(in);
+        // This assumes we're using Fixer.io and that the desired access key is
+        // in the properties file in the key labelled `fixer_io`.
+        accessKey = properties.getProperty("fixer_io");
     }
 
     public float getRate(JsonObject ratesInfo, String currency){
         return ratesInfo.getAsJsonObject("rates").get(currency).getAsFloat();
     }
 
+
+    public String padWithZero(int num){
+        if (num < 10) {
+            return "0" + String.valueOf(num);
+        } else {
+            return String.valueOf(num);
+        }
+    }
     /**
      * Get the exchange rate for the specified currency against the base
      * currency (the Euro) on the specified date.
@@ -58,9 +93,12 @@ public class ExchangeRateReader {
      */
     public float getExchangeRate(String currencyCode, int year, int month, int day) throws IOException {
         String urlString = base;
-        if(urlString.contains("faculty")){
-            urlString = urlString + "/" + year + "-" + month + "-" + day;
-        }
+
+        String monthString = padWithZero(month);
+        String dayString = padWithZero(day);
+
+        urlString = urlString + year + "-" + monthString + "-" + dayString + "?access_key=" + accessKey;
+
         URL url = new URL(urlString);
         InputStream inputStream = url.openStream();
         Reader reader = new InputStreamReader(inputStream);
@@ -87,13 +125,14 @@ public class ExchangeRateReader {
      * @return the desired exchange rate
      * @throws IOException
      */
-    public float getExchangeRate(
-            String fromCurrency, String toCurrency,
-            int year, int month, int day) throws IOException {
+    public float getExchangeRate(String fromCurrency, String toCurrency, int year, int month, int day) throws IOException {
         String urlString = base;
-        if(urlString.contains("faculty")){
-            urlString = urlString + year + "-" + month + "-" + day;
-        }
+
+        String monthString = padWithZero(month);
+        String dayString = padWithZero(day);
+
+        urlString = urlString + year + "-" + monthString + "-" + dayString + "?access_key=" + accessKey;
+
         URL url = new URL(urlString);
         InputStream inputStream = url.openStream();
         Reader reader = new InputStreamReader(inputStream);
